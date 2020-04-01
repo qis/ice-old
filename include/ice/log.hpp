0574be5c3ce8a536666369379eff1ec70a44de45
Alexej Harm <alexej.h@xiphos.de>
2020-04-01 14:08:04 +0200

    fixed log stream inheritence

--- include/ice/log.hpp
+++ include/ice/log.hpp
@@ -13,7 +13,7 @@ void remove(std::shared_ptr<ice::log::sink> sink);
 std::string format(time_point tp, bool date = true, bool milliseconds = true);
 std::string format(severity s, bool padding = true);
 
-class stream : public std::stringbuf, public std::ostream {
+class stream : public std::stringbuf, private std::ostream {
 public:
   explicit stream(severity severity);
 
